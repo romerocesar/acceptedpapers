7fe2e1fcfe62e047a0b79039a79161139257d674
cesar <cesar.romero@gmail.com>
2023-11-25 18:18:34 -0800

    add requirements, update readme, update fetcher iface

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ML Conference Paper Explorer 📄🔍
 
-Welcome to the ML Conference Paper Explorer! This project is all about making it easy to access and explore papers from major machine learning conferences. We scrape the abstracts, turn them into embeddings, and display them in a user-friendly interface where you can search and explore to your heart's content.
+Welcome to the ML Conference Paper Explorer! This project is all about
+making it easy to access and explore papers from major machine
+learning conferences. We scrape the abstracts, turn them into
+embeddings, and display them in a user-friendly interface where you
+can search and explore to your heart's content.
 
 ## Features 🌟
 - Scrape abstracts from major ML conference websites.
@@ -13,15 +17,19 @@ Welcome to the ML Conference Paper Explorer! This project is all about making it
 3. Run the Streamlit app and enter the URL of the desired ML conference.
 4. Explore the papers' abstracts through our easy-to-navigate interface.
 
-## To-Dos 📝
+## To-Do 📝
 - [ ] Implement embedding generation for the scraped abstracts.
 - [ ] Integrate embeddings into the frontend for enhanced search capabilities.
 - [ ] Expand the scraping capabilities to more ML conference websites.
 - [ ] Add more interactive elements to the UI for a richer user experience.
 - [ ] Optimize performance for handling large datasets.
+- [ ] implement a data layer for storing and retrieving the scraped data.
+- [ ] separate scraping from web app.
 
 ## Contributing 🤝
-Feel free to fork this repository and submit pull requests. Any contributions to improve the project or add new features are more than welcome!
+Feel free to fork this repository and submit pull requests. Any
+contributions to improve the project or add new features are more than
+welcome!
 
 ## License 📜
 This project is open-source and available under the [MIT License](LICENSE).
--- scrapers.py
+++ scrapers.py
@@ -1,5 +1,8 @@
-import requests
+import logging
+
 from bs4 import BeautifulSoup
+import requests
+
 
 class Scraper:
     def get_publications(self, url):
@@ -15,7 +18,7 @@ class ICCVScraper(Scraper):
         #dependency injection: when we create an instance of ICCVScraper, we pass it the fetcher it should use -> maybe we can make this dynamic?
         self.fetcher = fetcher
         self.num_papers_to_scrape = num_papers_to_scrape
-    
+
     #when called on an instance of ICCVScraper will use fetcher attribute + num of papers to scrape papers from the provided url
     def get_publications(self, url):
         response = requests.get(url)
@@ -25,36 +28,36 @@ class ICCVScraper(Scraper):
         papers = []
 
         arxiv_anchors = [anchor for anchor in soup.find_all('a') if 'arXiv' in anchor.text]
-        
+
         # If num_papers_to_scrape is defined, limit the number of papers
         if self.num_papers_to_scrape:
             arxiv_anchors = arxiv_anchors[:self.num_papers_to_scrape]
-        
+
         for anchor in arxiv_anchors:
             title = anchor.find_previous('dt').text.strip()
             link = anchor['href']
 
             arxiv_id = link.split('/')[-1]
 
-            abstract, content = self.fetcher.fetch_content(arxiv_id)
+            abstract, content = self.fetcher.fetch(arxiv_id)
             papers.append({'title': title, 'url': link, 'abstract': abstract, 'content': content})
 
         return papers
-    
+
 
     #open/closed principle:
     #keep things open for extension but closed for modification
-    #in practice: scraper and get_publications are abstract base classes: they provide a blueprint but not a concrete implementation. If we want to to scrape a new conference we dont modify the base class(closed for modification), we just create a new subclass (open to extension) 
+    #in practice: scraper and get_publications are abstract base classes: they provide a blueprint but not a concrete implementation. If we want to to scrape a new conference we dont modify the base class(closed for modification), we just create a new subclass (open to extension)
 
     #a module in the context of OOP can be a method, class, package or a whole app.
-    #in the contenxt of open/closed principle: a module is any contained piece of software that has a specific responsibility/behaviour. 
-    #the goal is to build modules in a way that their capabilities can be extended, without changing existing behavior 
+    #in the contenxt of open/closed principle: a module is any contained piece of software that has a specific responsibility/behaviour.
+    #the goal is to build modules in a way that their capabilities can be extended, without changing existing behavior
 
     #the 5 solid principles of OOP:
     #Single Responsibility Principle: a class should have only one responsibility -> ArxivFetcher has only the job of fetching content from Arxiv
-    #Open/Closed Principle: open for extension but closed for modification 
+    #Open/Closed Principle: open for extension but closed for modification
     #Liskov substitution Principle: children can always replace the parent class without chaning the behavior of the program
-    #Interface Segragation Principle: no client should be forced to depend on interfaces they dont use. 
+    #Interface Segragation Principle: no client should be forced to depend on interfaces they dont use.
     #           Basically: the abstract/base class for scrapers should only define/promise methods that all scrapers would use and need. Otherwise they would be forced to implement methods they dont need, its better to create another interface/abstract class in that case
     #Dependency Inversion Principle: instead of specific details, parts of the code should rely on general concepts. For example, ICCVScraper is dependent on the abstract fetcher and not the specifc implementation ArxivFetcher. It relies on the general concept of a fetcher.
 
